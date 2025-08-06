ec6b8f7f0259e5ac08a1f072864c96911f4b9c1d
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-08-06 09:38:11 +0200

    feat: snack 3 done

--- snack.js
+++ snack.js
@@ -14,3 +14,23 @@ const quadrato = (num) => num * num
 
 console.log(quadrato(5))
 
+
+// Snack 3
+
+const sottrazione = (num1, num2) => num1 - num2
+const moltiplicazione = (num1, num2) => num1 * num2 
+
+function eseguiOperazione(num1, num2, callback) {
+    const operazione = callback(num1, num2)
+    return operazione
+}
+
+// oppure
+
+const eseguiOperazione2 = (num1, num2, callback) => callback(num1, num2)
+
+
+console.log(eseguiOperazione(6,8, moltiplicazione))
+
+
+// Snack 4
