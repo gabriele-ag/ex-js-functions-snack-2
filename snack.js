15d05222198a7f6bbe860330ca26c899e5d896f2
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-08-06 10:37:41 +0200

    feat: init bonus

--- snack.js
+++ snack.js
@@ -97,8 +97,6 @@ conteggio1()
 
 // Snack 7
 
-// Crea una funzione che ferma un timer dopo un certo tempo
-// Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
 
 function eseguiEferma(msg, tempoavvio, tempostop) {
     let conteggio = tempoavvio
@@ -107,7 +105,6 @@ function eseguiEferma(msg, tempoavvio, tempostop) {
         console.log(msg)
         if (conteggio === tempostop) {
             clearInterval(intervallo)
-            console.log("Tempo scaduto")
         }
         }, 1000)
      return
